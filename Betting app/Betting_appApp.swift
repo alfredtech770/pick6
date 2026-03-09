6cb983c2306e6f0ae722cdb1afba25798858e8ff
alfredtech770 <ethan@alfredconcierge.app>
2026-03-09 18:40:08 -0400

    Initial Commit

---
+++ Betting app/Betting_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Betting_appApp.swift
+//  Betting app
+//
+//  Created by Ethan on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Betting_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
